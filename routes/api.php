854c3f110617971328aa91bf43168094b00e3678
sheenazien8 <sheenazien08@gmail.com>
2018-08-17 20:43:26 +0700

    jwt auth middleware or protection with middleware

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,11 @@
 
 Route::group(["middleware" => ["api"]], function() {
 
-  Route::post('/auth/signup', "AuthController@signup");
-  Route::post('/auth/signin', "AuthController@signin");
+	Route::post('/auth/signup', "AuthController@signup");
+	Route::post('/auth/signin', "AuthController@signin");
+
+	Route::group(['middleware' => ['jwt.auth']], function() {
+	    Route::get('/profile', "UserController@show");
+	});
 
 });
