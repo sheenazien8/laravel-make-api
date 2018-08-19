29b3d7130c6d36b11996cc279ed3f4b83d3ba7e8
sheenazien8 <sheenazien08@gmail.com>
2018-08-19 14:17:31 +0700

    update and delete with protect user_id

--- routes/api.php
+++ routes/api.php
@@ -6,10 +6,14 @@ Route::group(["middleware" => ["api"]], function() {
 	Route::post('/auth/signin', "AuthController@signin");
 
 	Route::group(['middleware' => ['jwt.auth']], function() {
+
 	    Route::get('/profile', "UserController@show");
 	    Route::get('/tutorial', "TutorialController@index");
 	    Route::get('/tutorial/show/{id}', "TutorialController@show");
 	    Route::post('/tutorial', "TutorialController@store");
+
+	    Route::put('/tutorial/update/{id}',"TutorialController@update");
+	    Route::delete('/tutorial/delete/{id}',"TutorialController@delete");
 	});
 
 });
